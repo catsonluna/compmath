e2d0bb5731e9807835c08f30c76445af6d580e27
KingRavo18 <144904011+KingRavo18@users.noreply.github.com>
2023-10-29 12:02:35 +0000

    a

--- pages/components/leaderboard.tsx
+++ pages/components/leaderboard.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import style from '@/styles/leaderboard.module.css'
 
-function Display(){
+function Display() {
+    // State variable to hold the leaderboard data.
     const [leaderboard, setLeaderboard] = useState([
         {
             username: "afs",
@@ -21,11 +22,15 @@ function Display(){
         }
     ])
 
+    // useEffect hook to sort the leaderboard when the component is first loaded.
     useEffect(() => {
+        // Create a sorted copy of the leaderboard array based on ELO scores in descending order.
         const sortedLeaderboard = [...leaderboard].sort((a, b) => b.elo - a.elo);
+        // Update the state variable to reflect the sorted leaderboard.
         setLeaderboard(sortedLeaderboard);
     }, []);
 
+    // Function to determine the ordinal suffix for a number (e.g., 1st, 2nd, 3rd).
     const ordinalSuffix = (i: number) => {
         let j = i % 10;
         let k = i % 100;
@@ -45,6 +50,7 @@ function Display(){
         <>
         {leaderboard.map((player, index) => {
             let className;
+            // Determine the CSS class to be applied to the row based on the player's rank.
             if (index === 0) {
                 className = style.first;
             } else if (index === 1) {
@@ -55,6 +61,7 @@ function Display(){
                 className = style.rest;
             }
             
+            // Render a row for each player in the leaderboard with their username and ELO score.
             return (
                 <div key={player.username} className={style.row}>
                     <h1 className={className}>{ordinalSuffix(index+1)}. {player.username}</h1>
@@ -66,4 +73,4 @@ function Display(){
     )
 }
 
-export default Display
+export default Display;
