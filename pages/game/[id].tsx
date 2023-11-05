5892215aef9b16734afdd92b60d025c62c56da27
Dragonbestgg1 <dragonbestgg1@gmail.com>
2023-11-05 21:23:43 +0200

    Timer kinda fixed

--- pages/game/[id].tsx
+++ pages/game/[id].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { useEffect, useState, useRef } from 'react'; // Import useRef
+import { useEffect, useState, useRef } from 'react';
 import styles from '@/styles/game.module.css';
 import Heart from '@/pages/components/heart';
 import Calc from '@/pages/components/calc';
@@ -13,9 +13,10 @@ export default function Home() {
     let data: any;
 
     const [counter, setCounter] = useState(30);
-    const [inputValue, setInputValue] = useState(''); // Add this line
-    const inputRef1 = useRef(null); // Create a ref for the first input field
-    const inputRef2 = useRef(null); // Create a ref for the second input field
+    const [inputValue, setInputValue] = useState('');
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+    const [restartKey, setRestartKey] = useState(0); // Add this line
 
     useEffect(() => {
         if (!loading) return;
@@ -34,7 +35,19 @@ export default function Home() {
             }));
             setLoading(false);
             const timer = setInterval(() => {
-                setCounter((currentCounter) => currentCounter > 0 ? currentCounter - 1 : 0);
+                setCounter((currentCounter) => {
+                    if (currentCounter > 0) {
+                        return currentCounter - 1;
+                    } else {
+                        // When the countdown hits 0, increment restartKey to force a remount
+                        setRestartKey(prevKey => prevKey + 1);
+                        // Reset the counter to 30 seconds after 10 seconds
+                        setTimeout(() => {
+                            setCounter(30);
+                        }, 10000);
+                        return 0;
+                    }
+                });
             }, 1000);
             return () => clearInterval(timer);
         };
@@ -52,15 +65,12 @@ export default function Home() {
     }, [ws]);
 
     useEffect(() => {
-        // Focus the input field once the component has mounted
         if (inputRef1.current) {
           inputRef1.current.focus();
-        //   console.log(inputRef1.current);
         }
       }, []);
     
       const handleChange = (event) => {
-        // Update inputValue when the user types into the input field
         setInputValue(event.target.value);
       }
 
@@ -69,40 +79,40 @@ export default function Home() {
     }
 
     return (
-        <div className={`${styles.main}`}>
-            <div>
-                <button onClick={() => {
-                    console.log('clicked');
-                    ws.send(JSON.stringify({
-                        type: 'equation',
-                        gameId: id,
-                        userId: 'later'
-                    }));
-                }}>Click me</button>
-            </div>
-            <div className={`${styles.main}`}>{/*main*/}
-                <div id="time" className={`${styles.time}`}>{/*time*/}
-                    <p className={`${styles.timer}`}>{counter} sec</p>
-                </div>
-                <div className={`${styles.eq}`}>{/*equations*/}
-                    <h1>{equation}</h1>
-                </div>
-          <div className={`${styles.calcs}`}>{/*calcs*/}
-              <div className={`${styles.boxing}`}>
-                <div className={`${styles.calc1}`}>{/*calc1*/}
-                  <div className={`${styles.res}`}>
-                    {/* Add ref to the first input field */}
-                    <input type="number" value={inputValue} className={`${styles.in}`} onChange={handleChange} placeholder="0" ref={inputRef1}/>
+        <>
+          <div className={`${styles.main}`}>
+              <div>
+                  <button onClick={() => {
+                      console.log('clicked');
+                      ws.send(JSON.stringify({
+                          type: 'equation',
+                          gameId: id,
+                          userId: 'later'
+                      }));
+                  }}>Click me</button>
+              </div>
+              <div className={`${styles.main}`} key={restartKey}>{/*main*/}
+                  <div id="time" className={`${styles.time}`}>{/*time*/}
+                      <p className={`${styles.timer}`}>{counter} sec</p>
                   </div>
-                    {/* Pass setInputValue as a prop to Calc */}
-                    <Calc inputValue={inputValue} setInputValue={setInputValue} disabled={false}/>
-                </div>
+                  <div className={`${styles.eq}`}>{/*equations*/}
+                      <h1>{equation}</h1>
+                  </div>
+            <div className={`${styles.calcs}`}>{/*calcs*/}
+                <div className={`${styles.boxing}`}>
+                  <div className={`${styles.calc1}`}>{/*calc1*/}
+                    <div className={`${styles.res}`}>
+                      <input type="number" value={inputValue} className={`${styles.in}`} onChange={handleChange} placeholder="0" ref={inputRef1}/>
+                    </div>
+                      <Calc inputValue={inputValue} setInputValue={setInputValue} disabled={false}/>
+                  </div>
 
 
-                <div>
-                  <p className={`${styles.titl}`}>Me:</p>
-                  <Heart num={7} />
-                </div>
+                  <div>
+                    <p className={`${styles.titl}`}>Me:</p>
+                    <Heart num={7} />
+                  </div>
+
               </div>
               <div className={`${styles.boxing1}`}>
                 <p className={`${styles.titl}`}>Enemy:</p>
@@ -119,5 +129,6 @@ export default function Home() {
             </div>
         </div>
     </div>
+    </>
   );
 }
